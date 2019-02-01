0d7e2c2a1e26dfec271ff8e100ce42420f5c593f
Oscar <oscar.monsreal1996@gmail.com>
2019-02-01 09:15:11 -0600

    Institute Controller created

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('/catalogue/{user?}',['middleware'=> 'adminCatalogue','uses' =>'catalogueController@index']);
+Route::resource('instituciones', 'InstitucionController');
