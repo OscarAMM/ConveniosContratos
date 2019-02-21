851a0de3717e0eb50de0b6bbab649b93411876db
Oscar <oscar.monsreal1996@gmail.com>
2019-02-21 11:15:17 -0600

    new view

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -3,44 +3,54 @@
 @section('content')
 
 
-  <!--  <h2 class="font-weight-bold text-center"> USUARIOS
+<!--  <h2 class="font-weight-bold text-center"> USUARIOS
     <a href="{{ route('admin.index') }}" class ="btn btn-primary pull-right">Nuevo</a>
     </h2> -->
-    @if(Auth::user()->hasRole('admin'))
-     
-    @include('auth.fragment.info')
+@if(Auth::user()->hasRole('admin'))
 
-    <div class="card">
-            <div class="card-header">
-                            {{Form::open(['route'=>'users.index','method'=>'GET','class'=>'form-inline'])}}
-                            <h4 class="text-muted font-weight-bold" style="margin-right:150px">CONSULTA DE USUARIOS</h4>
-                            <div class="form-group">
-                                {{Form::text('id',null,['class'=>'form-control','placeholder'=>'ID'])}}
-                            </div>
-                            <div class="form-group">
-                                {{Form::text('name',null,['class'=>'form-control','placeholder'=>'Nombre'])}}
-                            </div>
-                            <div class="form-group">
-                                {{Form::text('email',null,['class'=>'form-control','placeholder'=>'Email'])}}
-                            </div>
-                           
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary">
-                                    <span class="glyphicon glyphicon-search">BUSCAR</span>
-                                </button>
-                            </div>
-                        {{Form::close()}}
-                </div>
+@include('auth.fragment.info')
+<div class="card-header text-muted text-center" style="margin-bottom:5px">
+    <h3> Usuarios </h3>
+</div>
+{{Form::open(['route'=>'users.index','method'=>'GET','class'=>'form-inline'])}}
+<p class="text-item-center"><a href="{{route('admin.index')}}" class="btn btn-success"
+        style="margin-right:5px">Nuevo</a>
+    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample"
+        aria-expanded="false" aria-controls="collapseExample" style="margin-right:15px">
+        Búsqueda
+    </button></p>
+<div class="collapse" id="collapseExample">
+    <div class=" card card-body " style="margin-bottom:5px">
+        <!-- inicio form busqueda-->
+        <div class="form-row">
+            <div class="col" style="margin-right:5px">
+                {{Form::text('id',null,['class'=>'form-control','placeholder'=>'ID'])}}
             </div>
-    <div class= "card-body">
-    <table class= "table ">
-        <thead class= "thead-dark">
-        <tr>
-            <th>ID</th>
-            <th>Nombre</th>
-            <th>Email</th>
-            <th colspan="3">&nbsp;</th>
-        </tr>
+            <div class="col" style="margin-right:5px">
+                {{Form::text('name',null,['class'=>'form-control','placeholder'=>'Nombre'])}}
+            </div>
+            <div class="col" style="margin-right:5px">
+                {{Form::text('email',null,['class'=>'form-control','placeholder'=>'Email'])}}
+            </div>
+            <div class="col">
+                <button type="submit" class="btn btn-primary">
+                    <span class="glyphicon glyphicon-search">Buscar</span>
+                </button>
+            </div>
+        </div>
+    </div>
+    {{Form::close()}}
+</div>
+
+
+    <table class="table ">
+        <thead class="thead-dark">
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Email</th>
+                <th colspan="3">&nbsp;</th>
+            </tr>
         <tbody>
             @foreach($users as $user)
             <tr>
@@ -48,32 +58,32 @@
                 <td>{{$user->name}}</td>
                 <td>{{$user->email}}</td>
                 <td>
-                <a href="{{route('users.show', $user ->id)}}" class="btn btn-success">Ver</a> </td>
+                    <a href="{{route('users.show', $user ->id)}}" class="btn btn-success">Ver</a> </td>
                 <td>
-                <a href="{{route('users.edit', $user ->id)}}" class="btn btn-warning">Editar</a></td>
+                    <a href="{{route('users.edit', $user ->id)}}" class="btn btn-warning">Editar</a></td>
                 <td>
-                <form action="{{route('users.destroy', $user->id)}}" method="POST">
-                    {{csrf_field()}}
-                    <input type="hidden" name = "_method" value ="DELETE">
-                    <button class = "btn btn-danger">Eliminar</button>
-                </form>
+                    <form action="{{route('users.destroy', $user->id)}}" method="POST">
+                        {{csrf_field()}}
+                        <input type="hidden" name="_method" value="DELETE">
+                        <button class="btn btn-danger">Eliminar</button>
+                    </form>
                 </td>
             </tr>
             @endforeach
         </tbody>
         </thead>
     </table>
-    
-    {!!$users->render()!!}
-    </div>
-   
-    </div>
-    @else
-    <div class="container">
 
-            <h4>Acceso restringido</h4>
-            <h6>Comuníquese con su administrador</h6>
-    </div>
-    @endif
+    {!!$users->render()!!}
+</div>
+
+
+@else
+<div class="container">
+
+    <h4>Acceso restringido</h4>
+    <h6>Comuníquese con su administrador</h6>
+</div>
+@endif
 
 @endsection
--- resources/views/institutes/index.blade.php
+++ resources/views/institutes/index.blade.php
@@ -2,41 +2,52 @@
 
 @section('content')
 @if(Auth::user()->hasRole('admin'))
-    @include('institutes.fragment.info')
-    <div class="card">
-            <div class="card-header">
-            <h2 class="font-weight-bold text-center text-muted "> INSTITUCIONES <a href="{{route('Institute.create')}}" class ="btn btn-primary">Nuevo</a> </h2>
-                            {{Form::open(['route'=>'Institute.index','method'=>'GET','class'=>'form-inline'])}}
-                            <h4 class="text-muted font-weight-bold" style="margin-right:150px">CONSULTA DE INSTITUCIONES</h4>
-                            <div class="form-group" style="margin-right:5px">
-                                {{Form::text('id',null,['class'=>'form-control','placeholder'=>'ID'])}}
-                            </div>
-                            <div class="form-group" style ="margin-right:5px">
-                                {{Form::text('name',null,['class'=>'form-control','placeholder'=>'Nombre'])}}
-                            </div>
-                            <div class="form-group" style="margin-right:5px">
-                                {{Form::text('acronym',null,['class'=>'form-control','placeholder'=>'Siglas'])}}
-                            </div>
-                            <div class= "form-group" style="margin-right:5px">
-                                {{Form::text('country',null,['class' =>'form-control','placeholder'=>'Pais'])}}
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary">
-                                    <span class="glyphicon glyphicon-search">BUSCAR</span>
-                                </button>
-                            </div>
-                        {{Form::close()}}
-                </div>
+@include('institutes.fragment.info')
+<div class="card-header text-muted text-center" style="margin-bottom:5px">
+    <h3> Instituciones </h3>
+</div>
+{{Form::open(['route'=>'Institute.index','method'=>'GET','class'=>'form-inline'])}}
+<p class="text-item-center"><a href="{{route('Institute.create')}}" class="btn btn-success"
+        style="margin-right:5px">Nuevo</a>
+    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample"
+        aria-expanded="false" aria-controls="collapseExample" style="margin-right:15px">
+        Búsqueda
+    </button></p>
+<div class="collapse" id="collapseExample">
+    <div class=" card card-body " style="margin-bottom:5px">
+        <!-- inicio form busqueda-->
+        <div class="form-row">
+            <div class="col" style="margin-right:5px">
+                {{Form::text('id',null,['class'=>'form-control','placeholder'=>'ID'])}}
             </div>
-<table class= "table">
-    <thead class= "thead-dark">
-    <tr>
-        <th>ID</th>
-        <th>Nombre</th>
-        <th>Siglas</th>
-        <th>País</th>
-        <th colspan="3">&nbsp;</th>
-    </tr>
+            <div class="col" style="margin-right:5px">
+                {{Form::text('name',null,['class'=>'form-control','placeholder'=>'Nombre'])}}
+            </div>
+            <div class="col" style="margin-right:5px">
+                {{Form::text('acronym',null,['class'=>'form-control','placeholder'=>'Siglas'])}}
+            </div>
+            <div class="col" style="margin-right:5px">
+                {{Form::text('country',null,['class' =>'form-control','placeholder'=>'Pais'])}}
+            </div>
+            <div class="col">
+                <button type="submit" class="btn btn-primary">
+                    <span class="glyphicon glyphicon-search">Buscar</span>
+                </button>
+            </div>
+        </div>
+    </div>
+    {{Form::close()}}
+</div>
+
+<table class="table">
+    <thead class="thead-dark">
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Siglas</th>
+            <th>País</th>
+            <th colspan="3">&nbsp;</th>
+        </tr>
     <tbody>
         @foreach($institutions as $institute)
         <tr>
@@ -45,15 +56,15 @@
             <td>{{$institute->acronym}}</td>
             <td>{{$institute->country}}</td>
             <td>
-            <a href="{{route('Institute.show', $institute ->id)}}" class="btn btn-success">Ver</a> </td>
+                <a href="{{route('Institute.show', $institute ->id)}}" class="btn btn-success">Ver</a> </td>
             <td>
-            <a href="{{route('Institute.edit', $institute ->id)}}" class="btn btn-warning">Editar</a></td>
+                <a href="{{route('Institute.edit', $institute ->id)}}" class="btn btn-warning">Editar</a></td>
             <td>
-            <form action="{{route('Institute.destroy', $institute->id)}}" method="POST">
-                {{csrf_field()}}
-                <input type="hidden" name = "_method" value ="DELETE">
-                <button class = "btn btn-danger">Eliminar</button>
-            </form>
+                <form action="{{route('Institute.destroy', $institute->id)}}" method="POST">
+                    {{csrf_field()}}
+                    <input type="hidden" name="_method" value="DELETE">
+                    <button class="btn btn-danger">Eliminar</button>
+                </form>
             </td>
         </tr>
         @endforeach
@@ -63,12 +74,10 @@
 {!!$institutions->render()!!}
 </div>
 @else
-    <div class="container">
+<div class="container">
 
-            <h4>Acceso restringido</h4>
-            <h6>Comuníquese con su administrador</h6>
-    </div>
+    <h4>Acceso restringido</h4>
+    <h6>Comuníquese con su administrador</h6>
+</div>
 @endif
 @endsection
- 
-   
