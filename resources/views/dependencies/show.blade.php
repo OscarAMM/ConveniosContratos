aa9588f87f3caa8bedf75d88f15351126b7a8e22
Oscar <oscar.monsreal1996@gmail.com>
2019-02-22 11:33:07 -0600

    new version

---
+++ resources/views/dependencies/show.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+@if(Auth::user()->hasRole('admin'))
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class=" text-center font-weight-bold text-muted">DEPENDENCIA</h4>
+                </div>
+                <div class="card-body">
+                    <li class="list-group-item">
+                        <h5 class="card-title font-weight-bold">Nombre:</h5>
+                        {{$dependence ->name}}
+                    </li>
+                    <li class="list-group-item">
+                        <h5 class="card-title font-weight-bold">Siglas</h5>
+                        {{$dependence ->acronym}}
+                    </li>
+                    <li class="list-group-item">
+                        <h5 class="card-title font-weight-bold">País</h5>
+                        {{$dependence ->country}}
+                    </li>
+                    <li class="list-group-item text-center">
+                        <a href="{{route('Dependence.index')}}" class="btn btn-secondary">Regresar</a>
+                        <a href="{{route('Dependence.edit', $dependence->id)}}" class="btn btn-primary"> Editar</a></li>
+                    </ul>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@else
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h2 class="text-muted">Acceso restringido</h2>
+        </div>
+        <div class="card-body">
+            <h4>EL Usuario no tiene acceso a esta área, comuníquese con su administrador si desea realizar algún cambio.
+            </h4>
+        </div>
+
+    </div>
+</div>
+@endif
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,29 +11,24 @@
 |
 */
 
-
 Auth::routes();
-Route::get('/','HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 //Ruta Institute
-Route::resource('Institute','InstituteController');
+Route::resource('Institute', 'InstituteController');
 //Ruta DEPENDENCIA
 Route::resource('Dependence', 'DependenceController');
+//Ruta contrato
+Route::resource('Contract', 'ContractController');
 
 //RUTAS DE ADMIN
-Route::get('/registerAdmin',["as"=>"admin.index","uses"=>'RegisterAdminController@index']);
-Route::post('/registerAdminCreate',["as"=>"admin.create","uses"=>'RegisterAdminController@create']);
+Route::get('/registerAdmin', ['as' => 'admin.index', 'uses' => 'RegisterAdminController@index']);
+Route::post('/registerAdminCreate', ['as' => 'admin.create', 'uses' => 'RegisterAdminController@create']);
 //RUTAS DE ROLES
-Route::get('/registerAdminRoles','RegisterAdminController@change_roles');
-Route::post('/registerAdminRolesChange','RegisterAdminController@AdminAssignRoles');
-
+Route::get('/registerAdminRoles', 'RegisterAdminController@change_roles');
+Route::post('/registerAdminRolesChange', 'RegisterAdminController@AdminAssignRoles');
 
 //Route::post('/registerAdminRolesChange7{{$id}}','RegisterAdminController@AdminAssignRoles');
 //Route::get('/usersview/{id}',["as" => "users.view","uses" => "RegisterAdminController@AdminAssignRoles"]);
 Route::resource('users', 'UserController');
 
-Route::post('/usersEdited/{id}',["as" => "users.edited","uses" => "UserController@edited"]);
-
-
-
-
-
+Route::post('/usersEdited/{id}', ['as' => 'users.edited', 'uses' => 'UserController@edited']);
