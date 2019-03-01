26b506a27d0c6106a479d2c869b965f7469eec2a
Oscar <oscar.monsreal1996@gmail.com>
2019-03-01 12:41:31 -0600

    views h,cr,w deleted

--- resources/views/actions.blade.php
+++
@@ -1,3 +0,0 @@
-<a href="{{ route('users.show', $id) }}" class="btn btn-success btn-sm">Ver</a>
-<a href="{{ route('users.edit', $id) }}" class="btn btn-warning btn-sm">Editar</a>
-<a href="{{ route('users.destroy', $id) }}" class="btn btn-danger btn-sm">Eliminar</a>
