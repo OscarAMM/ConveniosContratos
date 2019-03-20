ccdba9f70713c7e598b411bcf0df953609059d17
Oscar <oscar.monsreal1996@gmail.com>
2019-03-20 10:18:32 -0600

    New design

--- resources/views/mail/mail.blade.php
+++ resources/views/mail/mail.blade.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SICC</title>
 </head>
+
 <body>
-<h5 style="color:#212529">Sistema de Convenio & Contrato</h5>
-{{$e_message}}
+    <div class="card">
+        <div class="card-header">
+            <h5 style="color:#212529">Sistema de Convenios & Contratos</h5>
+        </div>
+        <div class="card-body"> {{$e_message}}</div>
+    </div>
 </body>
+
 </html>
