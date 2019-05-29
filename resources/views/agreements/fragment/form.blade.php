09269363d0bbed582554dc47eac32c669bfdc826
David Diquer Ccid <davch987@gmail.com>
2019-05-28 19:37:18 -0500

    campos dinamicos sin funcionar

--- resources/views/agreements/fragment/form.blade.php
+++ resources/views/agreements/fragment/form.blade.php
@@ -70,7 +70,8 @@
                             <button type="button" class="btn btn-secondary col-sm-1" data-toggle="modal"
                             data-target="#suscrito" data-whatever="@fat">...</button>
                         </div>
-                        
+                        <div id="peopleList">
+                        </div>
                         <!--<select name="people_id" id="people_id"
                                 placeholder="Selecciona la institucion asignado" class="form-control"
                                 required="required">
@@ -78,8 +79,7 @@
                                 <option value="{{$person->id}}">{{$person->name}}</option>
                                 @endforeach
                             </select>-->
-                        <div id="peopleList">
-                        </div>
+                        
                     </div>
                     <div class="form-group">
                         <small style="color:#D90101;">*</small>
@@ -104,6 +104,15 @@
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <label for="file" class="col-form-label">Seleccione el archivo</label>
                         <input type="file" class="form-control-file" name="file" id="file">
+                    </div>
+                    <div class="field_wrapper">
+                        <div>
+                            <input type="text" class="autocomplete"name="field_name[]" id="field_name[]"value=""/>
+                            <a href="javascript:void(0);" class="add_button" title="Add field">Agregar</a>
+                            <div id="peopleList">
+                            </div>
+                        </div>
+                        
                     </div>
                     {{csrf_field()}}
                 </form>
@@ -273,5 +282,50 @@ jQuery(document).ready(function() {
             $("#end_date").val(value);
         });
 
+
+
+        /*___*/
+var maxField = 10; //Input fields increment limitation
+    var addButton = $('.add_button'); //Add button selector
+    var wrapper = $('.field_wrapper'); //Input field wrapper
+    var fieldHTML = '<div><input type="text" name="field_name[]" class="autocomplete" value=""/><a href="javascript:void(0);" class="remove_button" title="Remove field"> Eliminar</a></div>'; //New input field html 
+    var x = 1; //Initial field counter is 1
+    $(addButton).click(function(){ //Once add button is clicked
+        if(x < maxField){ //Check maximum number of input fields
+            x++; //Increment field counter
+            $(wrapper).append(fieldHTML); // Add field html
+        }
+    });
+    $(wrapper).on('click', '.remove_button', function(e){ //Once remove button is clicked
+        e.preventDefault();
+        $(this).parent('div').remove(); //Remove field html
+        x--; //Decrement field counter
+    });
+    $('.autocomplete').keyup(function() {
+        var query = $(this).val();
+        if (query != '') {
+            var _token = $('input[name="_token"]').val();
+            $.ajax({
+                url: "{{ route('autocomplete.fetch') }}",
+                method: "POST",
+                data: {
+                    query: query,
+                    _token: _token
+                },
+                success: function(data) {
+                    $('#peopleList').fadeIn();
+                    $('#peopleList').html(data);
+                }
+            });
+        }
+    });
+
+    jQuery('#peopleList').on('click', 'li', function() {
+        $('.autocomplete').val($(this).text());
+        $('#peopleList').fadeOut();
+    });
+
 });
+
+
 </script>
